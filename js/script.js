b4d14dbf3bd6a202b25ffe5ceede17fcca2cb35c
nafij-islam <sahariannafis70@gmail.com>
2025-06-24 21:30:11 +0600

    update

--- js/script.js
+++ js/script.js
@@ -2,14 +2,20 @@ let name=document.querySelector('.name')
 let caption=document.querySelector('.caption')
 let postbtn=document.querySelector('.postbtn')
 let allpost=document.querySelector('.allpost')
+let updatebtn=document.querySelector('.updatebtn')
+let error=document.querySelector('.error')
+let nopost=document.querySelector('.nopost')
 
 let all=[]
 
-
-
-
+/*
+*post button start
+*/
 postbtn.addEventListener("click", function(){
-    all.push({
+    if(!(name.value && caption.value)){
+        error.innerHTML="Name and Captiono Daw.!!"
+    }else{
+           all.push({
         name:name.value,
         caption:caption.value
     })
@@ -17,6 +23,14 @@ postbtn.addEventListener("click", function(){
      display()
      name.value=""
      caption.value=""
+     error.innerHTML=""
+
+    }
+    if(all.length>0){
+        nopost.innerHTML=""
+
+    }
+ 
    
 })
 
@@ -26,11 +40,28 @@ function display(){
                            <div class="card-body">
                               <h5 class="card-title">${item.name}</h5>
                               <p class="card-text">${item.caption}</p>
-                              <button>Edit</button>
-                              <button>Delete</button>
+                              <button class="edit">Edit</button>
+                              <button  class="delete">Delete</button>
                            </div>
                        </div>`
 })
+            
+
+
+            let deletebtn=document.querySelectorAll(".delete")
+            let convertDeleteBtn=Array.from(deletebtn)
+
+            convertDeleteBtn.map((item,index)=>{
+                item.addEventListener('click',function(){
+                   all.splice(index,1)
+                    allpost.innerHTML=""
+                   display()
+                })
+            })
+            
+           
+            
+          
 
 }
 
