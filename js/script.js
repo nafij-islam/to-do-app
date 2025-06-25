d58fa0d6138d9632123d0caad1ae4e4f7ae2b90e
nafij-islam <sahariannafis70@gmail.com>
2025-06-25 16:12:38 +0600

    update

--- js/script.js
+++ js/script.js
@@ -6,6 +6,8 @@ let updatebtn=document.querySelector('.updatebtn')
 let error=document.querySelector('.error')
 let nopost=document.querySelector('.nopost')
 
+let storeIndex;  
+
 let all=[]
 
 /*
@@ -34,6 +36,21 @@ postbtn.addEventListener("click", function(){
    
 })
 
+updatebtn.addEventListener('click',function(){
+    
+    
+    all[storeIndex].name=name.value
+    all[storeIndex].caption=caption.value
+    allpost.innerHTML=""
+    name.value=""
+    caption.value=""
+     updatebtn.style.display="none"
+     postbtn.style.display="block"
+    display()
+
+    
+})
+
 function display(){
     all.map(item=>{
         allpost.innerHTML+=`<div class="card" style="width: 18rem;">
@@ -47,7 +64,8 @@ function display(){
 })
             
 
-
+            //Delete part
+ 
             let deletebtn=document.querySelectorAll(".delete")
             let convertDeleteBtn=Array.from(deletebtn)
 
@@ -60,6 +78,24 @@ function display(){
             })
             
            
+            //Edit part
+            let edit=document.querySelectorAll('.edit')
+            let convertEditBtn=Array.from(edit)
+             convertEditBtn.map((item,index)=>{
+                item.addEventListener('click',function(){
+                   
+                    updatebtn.style.display="block"
+                    postbtn.style.display="none"
+                   
+                   
+                    name.value=all[index].name
+                    caption.value=all[index].caption
+
+                    storeIndex=index
+            
+                })
+             })
+
             
           
 
