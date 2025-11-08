c8dfded66d6958dce28a88f4045819ed43c66f7a
nafij islam <sahariannafis70@gmail.com>
2025-11-08 22:41:19 +0600

    Add

--- js/script.js
+++ js/script.js
@@ -1,103 +1,85 @@
-let name=document.querySelector('.name')
-let caption=document.querySelector('.caption')
-let postbtn=document.querySelector('.postbtn')
-let allpost=document.querySelector('.allpost')
-let updatebtn=document.querySelector('.updatebtn')
-let error=document.querySelector('.error')
-let nopost=document.querySelector('.nopost')
+let name = document.querySelector(".name");
+let caption = document.querySelector(".caption");
+let postbtn = document.querySelector(".postbtn");
+let allpost = document.querySelector(".allpost");
+let updatebtn = document.querySelector(".updatebtn");
+let error = document.querySelector(".error");
+let nopost = document.querySelector(".nopost");
 
-let storeIndex;  
+let storeIndex;
 
-let all=[]
+let all = [];
 
 /*
-*post button start
-*/
-postbtn.addEventListener("click", function(){
-    if(!(name.value && caption.value)){
-        error.innerHTML="Plz Name And Caption!!"
-    }else{
-           all.push({
-        name:name.value,
-        caption:caption.value
-    })
-     allpost.innerHTML=""
-     display()
-     name.value=""
-     caption.value=""
-     error.innerHTML=""
+ *post button start
+ */
+postbtn.addEventListener("click", function () {
+  if (!(name.value && caption.value)) {
+    error.innerHTML = "Plz Name And Caption!!";
+  } else {
+    all.push({
+      name: name.value,
+      caption: caption.value,
+    });
+    allpost.innerHTML = "";
+    display();
+    name.value = "";
+    caption.value = "";
+    error.innerHTML = "";
+  }
+  if (all.length > 0) {
+    nopost.innerHTML = "";
+  }
+});
 
-    }
-    if(all.length>0){
-        nopost.innerHTML=""
+updatebtn.addEventListener("click", function () {
+  all[storeIndex].name = name.value;
+  all[storeIndex].caption = caption.value;
+  allpost.innerHTML = "";
+  name.value = "";
+  caption.value = "";
+  updatebtn.style.display = "none";
+  postbtn.style.display = "block";
+  display();
+});
 
-    }
- 
-   
-})
-
-updatebtn.addEventListener('click',function(){
-    
-    
-    all[storeIndex].name=name.value
-    all[storeIndex].caption=caption.value
-    allpost.innerHTML=""
-    name.value=""
-    caption.value=""
-     updatebtn.style.display="none"
-     postbtn.style.display="block"
-    display()
-
-    
-})
-
-function display(){
-    all.map(item=>{
-        allpost.innerHTML+=`<div class="card" style="width: 18rem;">
+function display() {
+  all.map((item) => {
+    allpost.innerHTML += `<div class="card" style="width: 18rem;">
                            <div class="card-body">
                               <h5 class="card-title">${item.name}</h5>
                               <p class="card-text">${item.caption}</p>
                               <button class="edit">Edit</button>
                               <button  class="delete">Delete</button>
                            </div>
-                       </div>`
-})
-            
+                       </div>`;
+  });
 
-            //Delete part
- 
-            let deletebtn=document.querySelectorAll(".delete")
-            let convertDeleteBtn=Array.from(deletebtn)
+  //Delete part
 
-            convertDeleteBtn.map((item,index)=>{
-                item.addEventListener('click',function(){
-                   all.splice(index,1)
-                    allpost.innerHTML=""
-                   display()
-                })
-            })
-            
-           
-            //Edit part
-            let edit=document.querySelectorAll('.edit')
-            let convertEditBtn=Array.from(edit)
-             convertEditBtn.map((item,index)=>{
-                item.addEventListener('click',function(){
-                   
-                    updatebtn.style.display="block"
-                    postbtn.style.display="none"
-                   
-                   
-                    name.value=all[index].name
-                    caption.value=all[index].caption
+  let deletebtn = document.querySelectorAll(".delete");
+  let convertDeleteBtn = Array.from(deletebtn);
 
-                    storeIndex=index
-            
-                })
-             })
+  convertDeleteBtn.map((item, index) => {
+    item.addEventListener("click", function () {
+      all.splice(index, 1);
+      allpost.innerHTML = "";
+      display();
+    });
+  });
 
-            
-          
+  //Edit part
+  let edit = document.querySelectorAll(".edit");
+  let convertEditBtn = Array.from(edit);
+  convertEditBtn.map((item, index) => {
+    item.addEventListener("click", function () {
+      updatebtn.style.display = "block";
+      postbtn.style.display = "none";
 
+      name.value = all[index].name;
+      caption.value = all[index].caption;
+
+      storeIndex = index;
+    });
+  });
 }
-
